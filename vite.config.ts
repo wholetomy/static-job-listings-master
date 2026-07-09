90ff0324b2dd2250f18144971faa79b168f15de1
Thomas Campanholi <thomasaqw@live.com>
2026-07-09 20:34:13 -0300

    Added base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/static-job-listings-master/",
   server: {
     host: '0.0.0.0',
     watch: {
